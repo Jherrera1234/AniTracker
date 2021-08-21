fa8576916f3cbca27e5d85d133e0431ffb443654
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-08-20 21:37:40 -0500

    able to successfully retrieve data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { baseURL, config } from './services'
 
 function App() {
+  const [anime, setAnime] = useState([])
+  const [toggleFetch, setToggleFetch] = useState(false)
+
+  useEffect(() => {
+    const fetchAnime = async () => {
+      const resp = await axios.get(baseURL, config)
+      console.log(resp)
+    }
+    fetchAnime()
+  }, [])
+
   return (
     <>
 
---
+++ client/src/services/index.js
@@ -0,0 +1,13 @@
+//Define the api url
+
+export const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_APP}/Anime`
+
+//define a config object that has our authorization api key
+
+export const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+  }
+
+
+}
