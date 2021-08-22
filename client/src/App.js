660deb9607cdb01b352ed33de45cae2fcdff6394
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-08-22 12:43:15 -0500

    added links successfully to home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { baseURL, config } from './services'
-
+import { Route, Link } from 'react-router-dom'
 function App() {
   const [anime, setAnime] = useState([])
   const [toggleFetch, setToggleFetch] = useState(false)
@@ -9,13 +9,18 @@ function App() {
   useEffect(() => {
     const fetchAnime = async () => {
       const resp = await axios.get(baseURL, config)
-      console.log(resp)
+      console.log(resp.data.records)
+      setAnime(resp.data.records)
     }
     fetchAnime()
   }, [])
 
   return (
     <>
+      <nav class='nav-bar' >
+        <Link to='/' className='home-nav' >Home </Link>
+        <Link to='/new' className='new-naw'>Add New Anime</Link>
+      </nav>
 
     </>
   );
