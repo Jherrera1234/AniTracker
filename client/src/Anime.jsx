565ba6880b7ff3e0a3db521ed84c07cc78dc572b
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-08-22 13:13:07 -0500

    Rendered the Anime component successfully

---
+++ client/src/Anime.jsx
@@ -0,0 +1,14 @@
+import { Link, Route } from 'react-router-dom'
+import { baseURL, config } from './services'
+
+function Anime(props) {
+
+  return (
+    <h1>This is working</h1>
+  )
+
+
+
+}
+
+export default Anime
--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { baseURL, config } from './services'
 import { Route, Link } from 'react-router-dom'
+import Anime from './Anime'
+
 function App() {
   const [anime, setAnime] = useState([])
   const [toggleFetch, setToggleFetch] = useState(false)
@@ -21,6 +23,17 @@ function App() {
         <Link to='/' className='home-nav' >Home </Link>
         <Link to='/new' className='new-naw'>Add New Anime</Link>
       </nav>
+      <Route path='/' exact>
+        {anime.map((anime, index) => {
+          return (
+            <Anime key={index} anime={anime} setToggleFetch={setToggleFetch} />
+          )
+        })
+
+        }
+
+      </Route>
+
 
     </>
   );
